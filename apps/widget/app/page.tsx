5beedfc24c797c4cb88827da82e0c94be26870bf
Vignesh Gupta <vighneshgupta32@gmail.com>
2025-08-10 16:52:24 +0530

    chore: setup convex shared package

--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,12 +1,17 @@
-import { Button } from "@workspace/ui/components/button"
+"use client";
+
+import { api } from "@echo/backend/_generated/api";
+import { useQuery } from "convex/react";
 
 export default function Page() {
+  const users = useQuery(api.users.getMany);
+
   return (
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
         <h1 className="text-2xl font-bold">Hello app/widget</h1>
-        <Button size="sm">Button</Button>
+        <span>{JSON.stringify(users)}</span>
       </div>
     </div>
-  )
+  );
 }
--- apps/widget/next.config.mjs
+++ apps/widget/next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  transpilePackages: ["@workspace/ui"],
+  transpilePackages: ["@echo/ui"],
 }
 
 export default nextConfig
